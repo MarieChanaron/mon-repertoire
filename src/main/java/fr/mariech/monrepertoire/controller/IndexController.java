23fd91711a1b78d085e24dda3d5ace7209c2fcbf
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-09 14:48:41 +0200

    homepage done

---
+++ src/main/java/fr/mariech/monrepertoire/controller/IndexController.java
@@ -0,0 +1,14 @@
+package fr.mariech.monrepertoire.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
+        return "index";
+    }
+}
--- src/main/java/fr/mariech/monrepertoire/controller/UserController.java
+++ src/main/java/fr/mariech/monrepertoire/controller/UserController.java
@@ -3,10 +3,16 @@ package fr.mariech.monrepertoire.controller;
 import fr.mariech.monrepertoire.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserService service;
+
 }
