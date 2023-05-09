7fa8b7cbc755e6d2cc3405aad9e061aa4394a0cc
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-09 18:07:31 +0200

    change header index link given user is logged in or not

--- src/main/java/fr/mariech/monrepertoire/controller/ContactController.java
+++ src/main/java/fr/mariech/monrepertoire/controller/ContactController.java
@@ -1,10 +1,14 @@
 package fr.mariech.monrepertoire.controller;
 
+import fr.mariech.monrepertoire.entity.Contact;
 import fr.mariech.monrepertoire.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ContactController {
@@ -14,6 +18,7 @@ public class ContactController {
 
     @GetMapping("/contacts")
     public String contacts(Model model) {
+        model.addAttribute("contacts", new ArrayList<Contact>());
         return "contacts";
     }
 }
